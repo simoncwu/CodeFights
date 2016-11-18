2a882017f3bddeb19a7b2f583165ea7a5151c3b8
simoncwu <simon.c.wu@gmail.com>
2016-11-17 19:22:50 -0500

    Add unit tests and featured challenge "cardinal".

---
+++ CodeFights.CSharp.UnitTest/Challenges.cs
@@ -0,0 +1,76 @@
+﻿using CodeFights.CSharp.Challenges;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFights.CSharp.UnitTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class Challenges
+    {
+        public Challenges()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Cardinal()
+        {
+            var c = new Cardinal();
+            Assert.AreEqual("one thousand two hundred and fifty five", c.cardinal("1255"));
+            Assert.AreEqual("fifteen", c.cardinal("15"));
+            Assert.AreEqual("zero", c.cardinal("0"));
+            Assert.AreEqual("one billion one", c.cardinal("1000000001"));
+            Assert.AreEqual("one hundred and twenty three trillion four hundred and fifty six billion seven hundred and eighty nine million twelve thousand three hundred and forty five", c.cardinal("123456789012345"));
+            Assert.AreEqual("four hundred and fifty eight million", c.cardinal("458000000"));
+            Assert.AreEqual("nine hundred and ninety nine trillion nine hundred and ninety nine billion nine hundred and ninety nine million nine hundred and ninety nine thousand nine hundred and ninety nine", c.cardinal("999999999999999"));
+            Assert.AreEqual("ten billion", c.cardinal("10000000000"));
+        }
+    }
+}
---
+++ CodeFights/Challenges/Cardinal.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFights.CSharp.Challenges
+{
+    /// <summary>
+    /// Solution for CodeFights challenge "cardinal": 
+    /// https://codefights.com/challenge/XRGXNwsdbBrvp66jM
+    /// 
+    /// Given a number n, convert it into its full cardinal form.
+    /// </summary>
+    public class Cardinal
+    {
+        /// <summary>
+        /// Converts a number into a cardinal numeral.
+        /// </summary>
+        /// <param name="n">A string containing a non-negative integer without leading zeros. 1 ≤ n.length ≤ 15.</param>
+        /// <returns>The cardinal numeral representation of <paramref name="n"/>.</returns>
+        /// <example>
+        /// For n = "1255", the output should be <c>cardinal(n) = "one thousand two hundred and fifty five"</c>.
+        /// </example>
+        public string cardinal(string n)
+        {
+            // special condition for just 0
+            if (n == "0") return "zero";
+
+            n = n.TrimStart('0');
+            var sb = new StringBuilder();
+            var s = "";
+            int l = n.Length;
+
+            // hundreds and up
+            if (l > 2)
+            {
+                // most significant group of digits
+                int digits = l == 3 ? 1 : (l - 1) % 3 + 1;
+                s = n.Substring(0, digits);
+                if (int.Parse(s) > 0)
+                    sb.Append(cardinal(s) + " " + (l > 6 ? ((l > 12 ? "tri" : l > 9 ? "bi" : "mi") + "llion") : l > 3 ? "thousand" : "hundred") + " ");
+
+                // remaining digits
+                s = n.Substring(digits);
+                if (l == 3 && int.Parse(s) > 0) // only append "and" after hundreds
+                    sb.Append("and ");
+
+                sb.Append(cardinal(s));
+            }
+            else if (l > 1)
+            {
+                if (n[0] == '1')
+                {
+                    // teens
+                    switch (n[1])
+                    {
+                        case '0':
+                            s = "ten";
+                            break;
+                        case '1':
+                            s = "eleven";
+                            break;
+                        case '2':
+                            s = "twelve";
+                            break;
+                        case '3':
+                            s = "thirteen";
+                            break;
+                        case '4':
+                            s = "fourteen";
+                            break;
+                        case '5':
+                            s = "fifteen";
+                            break;
+                        case '6':
+                            s = "sixteen";
+                            break;
+                        case '7':
+                            s = "seventeen";
+                            break;
+                        case '8':
+                            s = "eighteen";
+                            break;
+                        case '9':
+                            s = "nineteen";
+                            break;
+                    }
+                    sb.Append(s + " ");
+                }
+                else
+                {
+                    // multiples of ten
+                    switch (n[0])
+                    {
+                        case '2':
+                            s = "twen";
+                            break;
+                        case '3':
+                            s = "thir";
+                            break;
+                        case '4':
+                            s = "for";
+                            break;
+                        case '5':
+                            s = "fif";
+                            break;
+                        case '6':
+                            s = "six";
+                            break;
+                        case '7':
+                            s = "seven";
+                            break;
+                        case '8':
+                            s = "eigh";
+                            break;
+                        case '9':
+                            s = "nine";
+                            break;
+                    }
+                    sb.AppendFormat("{0}ty {1}", s, cardinal(n[1]));
+                }
+            }
+            else if (l > 0)
+            {
+                // single digit
+                sb.Append(cardinal(n[0]));
+            }
+            return sb.ToString().Trim();
+        }
+
+        /// <summary>
+        /// Converts a digit into a cardinal numeral.
+        /// </summary>
+        /// <param name="d">The digit to convert.</param>
+        /// <returns>The cardinal numeral representation of <paramref name="d"/>.</returns>
+        /// <remarks>This method returns <c>null</c> for the number 0 instead of "zero".</remarks>
+        string cardinal(char d)
+        {
+            switch (d)
+            {
+                case '1':
+                    return "one";
+                case '2':
+                    return "two";
+                case '3':
+                    return "three";
+                case '4':
+                    return "four";
+                case '5':
+                    return "five";
+                case '6':
+                    return "six";
+                case '7':
+                    return "seven";
+                case '8':
+                    return "eight";
+                case '9':
+                    return "nine";
+            }
+            return null;
+        }
+    }
+}
