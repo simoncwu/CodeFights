406b7816dd6e5837d899ce4ac466aae135aef12a
Simon <simon.c.wu@gmail.com>
2017-06-26 16:47:36 -0400

    Add reverse challenge "iDontKnow".

---
+++ CodeFights.CSharp.UnitTest/Challenges/Reverse/IDontKnowTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeFights.CSharp.Challenges.Reverse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFights.CSharp.Challenges.Reverse.Tests
+{
+    [TestClass()]
+    public class IDontKnowTests
+    {
+        [TestMethod()]
+        public void iDontKnowTest()
+        {
+            var i = new IDontKnow();
+            Assert.AreEqual("2", i.iDontKnow(2));
+            Assert.AreEqual("9900", i.iDontKnow(100));
+            Assert.AreEqual("46440", i.iDontKnow(216));
+            Assert.AreEqual("20306", i.iDontKnow(143));
+            Assert.AreEqual("9999700002", i.iDontKnow(99999));
+            Assert.AreEqual("152386680", i.iDontKnow(12345));
+            Assert.AreEqual("999999997000000000", i.iDontKnow(999999999));
+        }
+    }
+}
---
+++ CodeFights/Challenges/Reverse/IDontKnow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFights.CSharp.Challenges.Reverse
+{
+    /// <summary>
+    /// Solution for CodeFights reverse challenge "iDontKnow": 
+    /// https://codefights.com/challenge/xAfWeNKqLqeerhKXy
+    /// </summary>
+    public class IDontKnow
+    {
+        /// <summary>
+        /// Returns the product of a nonnegative integer and the integer that is one less than it, to floating point accuracy.
+        /// </summary>
+        /// <param name="number">A nonnegative integer.</param>
+        /// <returns>A string representing <paramref name="number"/> times <paramref name="number"/> minus <c>1</c>, to floating point accuracy.</returns>
+        public string iDontKnow(int number)
+        {
+            return Convert.ToInt64(Convert.ToDouble(number) * ~-number) + "";
+        }
+
+    }
+}
