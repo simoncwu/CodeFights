2abc48429376882f4dee4d24c5108f2baad75a86
Simon <simon.c.wu@gmail.com>
2017-06-27 09:44:31 -0400

    Add CodeFights interview practice "areFollowingPatterns".

---
+++ CodeFights.CSharp.UnitTest/InterviewPractice/AreFollowingPatternsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeFights.CSharp.InterviewPractice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFights.CSharp.InterviewPractice.Tests
+{
+    [TestClass()]
+    public class AreFollowingPatternsTests
+    {
+        [TestMethod()]
+        public void areFollowingPatternsTest()
+        {
+            var a = new AreFollowingPatterns();
+            Assert.IsTrue(a.areFollowingPatterns("cat dog dog".Split(), "a b b".Split()));
+            Assert.IsFalse(a.areFollowingPatterns("cat dog doggy".Split(), "a b b".Split()));
+            Assert.IsFalse(a.areFollowingPatterns("cat dog dog".Split(), "a b c".Split()));
+            Assert.IsTrue(a.areFollowingPatterns("aaa".Split(), "aaa".Split()));
+            Assert.IsFalse(a.areFollowingPatterns("aaa aaa aaa".Split(), "aaa bbb aaa".Split()));
+            Assert.IsFalse(a.areFollowingPatterns("aaa aab aaa".Split(), "aaa aaa aaa".Split()));
+            Assert.IsTrue(a.areFollowingPatterns("re jjinh rnz frok frok hxytef hxytef frok".Split(), "kzfzmjwe fgbugiomo ocuijka gafdrts gafdrts ebdva ebdva gafdrts".Split()));
+            Assert.IsFalse(a.areFollowingPatterns("kwtfpzm kwtfpzm kwtfpzm kwtfpzm kwtfpzm wfktjrdhu anx kwtfpzm".Split(), "z z z hhwdphhnc zejhegjlha xgxpvhprdd e u".Split()));
+            Assert.IsFalse(a.areFollowingPatterns("ato ato jflywws ato ato se se kiolm wizdkdqke".Split(), "ofnmiqelt ofnmiqelt flqmwoje ofnmiqelt zdohw jyk ujddjtxt s kw".Split()));
+            Assert.IsTrue(a.areFollowingPatterns("syf syf oxerkx oxerkx syf xgwatff pmnfaw t ajyvgwd xmhb ajg syf syf wjddgkopae fgrpstxd t i psw wjddgkopae wjddgkopae oxerkx zf jvdtdxbefr rbmphtrmo syf yssdddhyn syf jvdtdxbefr funnd syf syf wd syf vnntavj wjddgkopae yssdddhyn wcvk wjddgkopae fh zf gpkdcwf qkbw zf teppnr jvdtdxbefr fmn i hzmihfrmq wjddgkopae syf vnntavj dung kn qkxo ajyvgwd fs kanixyaepl syf tl yzhaa dung wa syf jtucivim tl kanixyaepl oxerkx wjddgkopae ey ai zf di oxerkx dung i oxerkx wmtqpwzgh t beascd me akklwhtpi nxl cnq bighexy ddhditvzdu funnd wmt dgx fs xmhb qtcxvdcl thbmn pkrisgmr mkcfscyb x oxerkx funnd iesr funnd t".Split(), "enrylabgky enrylabgky dqlqaihd dqlqaihd enrylabgky ramsnzhyr tkibsntkbr l bgtws xwuaep o enrylabgky enrylabgky e auljuhtj l d jfzokgt e e dqlqaihd fgglhiedk nj quhv enrylabgky oadats enrylabgky nj zwupro enrylabgky enrylabgky pyw enrylabgky bedpuycdp e oadats i e fobyfznrxm fgglhiedk irxtd oyvf fgglhiedk ebpp nj p d cufxylz e enrylabgky bedpuycdp mitzb shsnw papmvh bgtws chtp pze enrylabgky klp wpx mitzb fo enrylabgky bvcigrirhe klp pze dqlqaihd e iufunacwjo bubgww fgglhiedk og dqlqaihd mitzb d dqlqaihd mysidv l naj clftmrwl fjb zjjnrffb sh gcn ouispza zwupro c rdank chtp xwuaep jufhm iyntbgm sufs mkivpe bxdd dqlqaihd zwupro vzxbbculgv zwupro l".Split()));
+            Assert.IsFalse(a.areFollowingPatterns("a b c d a b c".Split(), "1 2 3 4 5 6 7".Split()));
+        }
+    }
+}
---
+++ CodeFights/InterviewPractice/AreFollowingPatterns.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFights.CSharp.InterviewPractice
+{
+    /// <summary>
+    /// Solution for CodeFights interview practice "areFollowingPatterns": 
+    /// https://codefights.com/interview-practice/task/3PcnSKuRkqzp8F6BN
+    /// Asked By: Google
+    /// 
+    /// Determine if an array follows the same pattern as another array. That 
+    /// is, for all possible indices i and j, strings[i] = strings[j] if and 
+    /// only if  patterns[i] = patterns[j].
+    /// </summary>
+    public class AreFollowingPatterns
+    {
+        /// <summary>
+        /// Indicates whether or not an array follows a specified pattern.
+        /// </summary>
+        /// <param name="strings">An array of strings.</param>
+        /// <param name="patterns">An array of pattern strings.</param>
+        /// <returns><c>true</c> if <paramref name="strings"/> follows pattern <paramref name="patterns"/>.</returns>
+        public bool areFollowingPatterns(string[] strings, string[] patterns)
+        {
+            var stringMap = new Dictionary<string, string>();
+            var patternMap = new Dictionary<string, string>();
+            for (int i = 0; i < strings.Length; i++)
+            {
+                var s = strings[i];
+                var p = patterns[i];
+
+                // check for mismatch
+                if (stringMap.ContainsKey(s) && stringMap[s] != p ||
+                   patternMap.ContainsKey(p) && patternMap[p] != s)
+                    return false;
+
+                // add new (or re-add existing) mapping
+                stringMap[s] = p;
+                patternMap[p] = s;
+            }
+            return true;
+        }
+
+    }
+}
