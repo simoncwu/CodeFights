3e3feb9a55c32f50c5c6f064a78ae7a30c2ea91b
Simon <simon.c.wu@gmail.com>
2017-07-25 09:06:43 -0400

    Add solution to challenge "minesweeper1".

---
+++ CodeFights.CSharp.UnitTest/Challenges/Minesweeper1Tests.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeFights.CSharp.Challenges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFights.CSharp.UnitTest;
+
+namespace CodeFights.CSharp.Challenges.Tests
+{
+    [TestClass()]
+    public class Minesweeper1Tests
+    {
+        [TestMethod()]
+        public void minesweeper1Test()
+        {
+            var m = new Minesweeper1();
+            var gameboard = Util.ParseJaggedArray(@"[[0,0,0,0,0], 
+ [0,1,1,1,0], 
+ [0,1,9,1,0], 
+ [0,1,1,1,0], 
+ [0,0,0,0,0]]", int.Parse);
+            Assert.IsTrue(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[0,0,0], 
+ [0,0,0], 
+ [0,0,0]]", int.Parse);
+            Assert.IsTrue(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[0,1,9,1], 
+ [0,1,1,1], 
+ [0,0,0,0]]", int.Parse);
+            Assert.IsTrue(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[9,9,9], 
+ [9,9,9], 
+ [9,9,9]]", int.Parse);
+            Assert.IsTrue(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[1,1,1], 
+ [1,1,1], 
+ [1,1,1]]", int.Parse);
+            Assert.IsFalse(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[0,1,2], 
+ [3,4,5], 
+ [6,7,8]]", int.Parse);
+            Assert.IsFalse(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[9,2,9], 
+ [2,4,2], 
+ [9,2,9]]", int.Parse);
+            Assert.IsTrue(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[9,2,9], 
+ [2,9,2], 
+ [9,2,9]]", int.Parse);
+            Assert.IsFalse(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[9,3,9], 
+ [3,9,3], 
+ [9,3,9]]", int.Parse);
+            Assert.IsTrue(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[9,9,9], 
+ [9,8,9], 
+ [9,9,9]]", int.Parse);
+            Assert.IsTrue(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[0,0,0,1,1], 
+ [0,0,0,1,9], 
+ [0,0,0,2,1], 
+ [0,0,0,1,9], 
+ [0,0,0,1,1]]", int.Parse);
+            Assert.IsFalse(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[9,1,1,9], 
+ [1,1,1,1], 
+ [1,1,1,1], 
+ [9,1,1,9]]", int.Parse);
+            Assert.IsTrue(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[0,0,0,0,0,0,0,0,0,0], 
+ [0,0,0,0,0,1,1,1,0,0], 
+ [0,0,0,0,0,1,9,1,0,0], 
+ [0,0,0,0,0,1,2,1,0,0], 
+ [1,1,0,0,0,0,0,1,1,1], 
+ [9,1,0,0,0,0,0,1,9,1]]", int.Parse);
+            Assert.IsFalse(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[0,0,1,9], 
+ [1,0,1,1], 
+ [1,0,0,0], 
+ [0,0,0,0]]", int.Parse);
+            Assert.IsFalse(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[0,0,1,9], 
+ [1,0,1,1], 
+ [0,0,0,0], 
+ [0,0,0,0]]", int.Parse);
+            Assert.IsFalse(m.minesweeper1(gameboard));
+
+            gameboard = Util.ParseJaggedArray(@"[[0,0,1,9], 
+ [0,0,1,1], 
+ [0,0,0,0], 
+ [0,0,0,0]]", int.Parse);
+            Assert.IsTrue(m.minesweeper1(gameboard));
+        }
+    }
+}
---
+++ CodeFights/Challenges/Minesweeper1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFights.CSharp.Challenges
+{
+    /// <summary>
+    /// Solution for CodeFights challenge "minesweeper1": 
+    /// https://codefights.com/challenge/ZXRv42mfhbiTTDrEB
+    /// 
+    /// Each cell of a Minesweeper gameboard can be either a mine (represented 
+    /// as 9) or the number of mines in its surrounding cells (0 to 8). Given 
+    /// the contents of a gameboard, determine whether or not the counts in all 
+    /// non-mine cells are accurate.
+    /// </summary>
+    public class Minesweeper1
+    {
+        /// <summary>
+        /// Indicates whether or not a Minesweeper gameboard is valid.
+        /// </summary>
+        /// <param name="gameboard">A rectangular matrix representing the gameboard.</param>
+        /// <returns><c>true</c> if <paramref name="gameboard"/> is valid; otherwise, <c>false</c>.</returns>
+        public bool minesweeper1(int[][] gameboard)
+        {
+            return gameboard.Select((_, r) => _.Select((v, c) =>
+            {
+                for (int i = 9; v < 9 & i-- > 0;)
+                {
+                    try
+                    {
+                        v -= gameboard[r - 1 + i / 3][c - 1 + i % 3] / 9;
+                    }
+                    catch { }
+                }
+                return v % 9;
+            }).Sum()).Sum() == 0;
+        }
+    }
+}
